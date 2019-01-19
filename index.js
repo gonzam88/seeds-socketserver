dc5c7eb4ffdf65d2331e8cac93017e0900913be2
gonz <gonzamoiguer@gmail.com>
2019-01-18 21:39:35 -0300

    agregado contraseña desde archivo. si ya se que no es muy seguro XD

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ var Victor = require('victor');
 var SelfReloadJSON = require('self-reload-json');
 const fs = require('fs');
 
+var secrets
+try {
+    secrets = require('./secrets.json');
+}
+catch (e) {
+    console.log("No existe el archivo de secretos. No se podrá acceder con contraseña")
+}
+
+
 var config = new SelfReloadJSON( path.join(__dirname, 'config.json'));
 config.on('updated', function(json) {
     console.log("config updated.");
@@ -176,7 +185,7 @@ wss.on('connection', function connection(ws, req) {
                     break;
 
                 case "changeClientOptions":
-                    if(typeof data.password == "undefined" || data.password !== process.env.OPTIONSPASSWORD) {console.log("contraseña incorrecta"); return;}
+                    if(typeof data.password == "undefined" || data.password !== secrets.password) {console.log("contraseña incorrecta"); return;}
                     fs.writeFile("config.json", JSON.stringify(data.config, undefined, 4), function(err) {
                         if(err) {
                             return console.log(err);
@@ -185,7 +194,7 @@ wss.on('connection', function connection(ws, req) {
                     });
                     break;
                 case "saltearArtista":
-                    if(typeof data.password == "undefined" || data.password !== process.env.OPTIONSPASSWORD) {console.log("contraseña incorrecta"); return;}
+                    if(typeof data.password == "undefined" || data.password !== secrets.password) {console.log("contraseña incorrecta"); return;}
                     currArtist.isDrawing = false;
                     currArtist.send(JSON.stringify({action:"stopartista"}))
                     currArtist = undefined;
@@ -194,7 +203,7 @@ wss.on('connection', function connection(ws, req) {
 
                     break;
                 case "borrarDibujo":
-                    if(typeof data.password == "undefined" || data.password !== process.env.OPTIONSPASSWORD) {console.log("contraseña incorrecta"); return;}
+                    if(typeof data.password == "undefined" || data.password !== secrets.password) {console.log("contraseña incorrecta"); return;}
                     // LEs mando a todo que borren el dibujo
                     wss.clients.forEach(function each(client) {
                       if (client.role !== "plottersecreto" && client.readyState === WebSocket.OPEN) {
